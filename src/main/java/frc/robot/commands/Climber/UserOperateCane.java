1c0992f79f1d3cb46a29c69b10a8daedd859f039
FredProbst <github@fsd.probst.org>
2022-03-22 16:36:27 -0400

    fixes uncompleted merge conflict
    This commit fixes an uncompleted merge conflict pushed
    to github.

--- src/main/java/frc/robot/commands/Climber/UserOperateCane.java
+++ src/main/java/frc/robot/commands/Climber/UserOperateCane.java
@@ -60,20 +60,11 @@ public class UserOperateCane extends CommandBase {
   }
 
   private void extendCane() {
-<<<<<<< HEAD
-    XboxController xbox = m_oi.getXbox();
-    
-    double extensionSpeed = 0;
-    if (xbox.getDPad().getDown().get()) {
-      extensionSpeed = m_caneExtensionSpeed;
-    } else if (xbox.getDPad().getUp().get()) {
-=======
     XboxController xbox = m_oi.getOperatorXboxController();
     double extensionSpeed = 0;
     if (xbox.getDPad().getUp().getAsBoolean()) { 
       extensionSpeed = m_caneExtensionSpeed;
     } else if (xbox.getDPad().getDown().getAsBoolean()) { 
->>>>>>> f664e025ca28b860b7df991ea59faaff11527073
       extensionSpeed = -m_caneExtensionSpeed;
     }
 
@@ -82,17 +73,11 @@ public class UserOperateCane extends CommandBase {
 
   private void rotateCane() {
     double rotationSpeed = 0;
-<<<<<<< HEAD
-    XboxController xbox = m_oi.getXbox();
-    if (xbox.getDPad().getRight().get()) {
-      rotationSpeed = m_caneRotationSpeed;
-    } else if (xbox.getDPad().getLeft().get()) {
-=======
+
     XboxController xbox = m_oi.getOperatorXboxController();
     if (xbox.getDPad().getLeft().getAsBoolean()) { 
       rotationSpeed = m_caneRotationSpeed;
-    } else if (xbox.getDPad().getRight().getAsBoolean()) { 
->>>>>>> f664e025ca28b860b7df991ea59faaff11527073
+    } else if (xbox.getDPad().getRight().getAsBoolean()) {  
       rotationSpeed = -m_caneRotationSpeed;
     }
     m_climber.rotateBothCanes(rotationSpeed);
