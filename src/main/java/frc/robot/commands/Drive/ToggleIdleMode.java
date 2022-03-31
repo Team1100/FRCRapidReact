be6485294e1a1e23a0d97bf888ca80c8acd4ca09
Zach Egner <egner.zach@gmail.com>
2022-03-30 20:52:47 -0400

    fixes ToggleIdleMode

--- src/main/java/frc/robot/commands/Drive/ToggleIdleMode.java
+++ src/main/java/frc/robot/commands/Drive/ToggleIdleMode.java
@@ -4,29 +4,61 @@
 
 package frc.robot.commands.Drive;
 
+import com.revrobotics.CANSparkMax.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import frc.robot.OI;
+import frc.robot.subsystems.Drive;
+import frc.robot.testingdashboard.TestingDashboard;
+
 public class ToggleIdleMode extends CommandBase {
+
+  private Drive m_drive;
+  private boolean m_parameterized;
+  private OI m_oi;
+  private boolean m_finished;
+  private IdleMode m_idleMode;
+
   /** Creates a new ToggleIdleMode. */
-  public ToggleIdleMode() {
+  public ToggleIdleMode(IdleMode idleMode) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_drive = Drive.getInstance();
+    addRequirements(m_drive);
+    m_finished = false;
+    m_idleMode = idleMode;
+  }
+
+  //Register with TestingDashboard
+  public static void registerWithTestingDashboard() {
+    Drive drive = Drive.getInstance();
+    ToggleIdleMode cmd = new ToggleIdleMode(IdleMode.kCoast);
+    TestingDashboard.getInstance().registerCommand(drive, "Basic", cmd);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_oi = OI.getInstance();
+    m_finished = false;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_drive.setIdleMode(m_idleMode);
+    m_finished = true;
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_finished = false;
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_finished;
   }
 }
